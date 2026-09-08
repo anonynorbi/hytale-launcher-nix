a8cc3ca2db257dd68f76c8be8e49b42553e8fbfe
anonynorbi <96613061+anonynorbi@users.noreply.github.com>
2026-09-08 19:40:10 +0000

    chore: update hytale-launcher to 2026.09.08-e1d69dd

--- package.nix
+++ package.nix
@@ -2,8 +2,8 @@
 
 let
   # === AUTO-UPDATE MARKERS - DO NOT MODIFY FORMAT ===
-  version = "2026.08.28-3d62362";
-  sha256 = "sha256-DLFvaRSfwilOkkdOz4rcnmsQTFQZvtIITmFBfhV67hg=";
+  version = "2026.09.08-e1d69dd";
+  sha256 = "sha256-IR33bZ3JT+LnGIorDO97C4513FQNbrKD5DOCQBQc8eg=";
   # === END AUTO-UPDATE MARKERS ===
 
   pname = "hytale-launcher";
